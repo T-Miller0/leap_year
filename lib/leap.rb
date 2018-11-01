c92c3afc5cfa9bc08f5b783a7be55ac791ca4c41
tom miller <thomas.j.c.miller@outlook.com>
2018-11-01 12:36:41 +0000

    div by 4 not by 100

--- lib/leap.rb
+++ lib/leap.rb
@@ -6,11 +6,12 @@ class Leap_year
   end
 
   def div_100_not_leap(year)
-    year % 100 == 0 && year % 400 == !0
+    year % 100 == 0 && year % 400 != 0
     return false
   end
 
-  def div_4_not_100
+  def div_4_not_100(year)
+    year % 4 == 0 && year % 100 != 0
   end
 
 end
